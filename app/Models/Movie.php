1c87118772c1e79b9050136b7e5998c837143a69
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2022-04-10 22:21:09 -0300

    fix: excluindo para teste de relações posteriores

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'duration',
+        'language',
+        'releaseDate',
+        'Country',
+        'Genre'
+    ];
 }
